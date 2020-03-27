8228895e28152d08e4d6039379a477e33648f5d4
vikash <vikashk@xpms.io>
2020-03-27 13:22:30 +0530

    updated newfunction, deleted angshu test

--- function_kk/new_function.py
+++ function_kk/new_function.py
@@ -1,3 +1,5 @@
 def new_function():
     print("COVID-19")
     print("no problem")
+    print("COVID-19")
+    print("no problem")
--- function_test/roadmap_sample.py
+++
@@ -1,3 +0,0 @@
-def roadmap_sample():
-    print("COVID-19")
-    print("update number 1")
